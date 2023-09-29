dc1df687a7ea2504911ce1be8a2a1fc3034715c4
El Thaariq Is'ad <eriq.isad@gmail.com>
2023-09-29 14:37:44 +0700

    Changes again...

---
+++ resources/views/universitas/mahasiswa.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ol>
+        <li><?php echo $mahasiswa01?></li>
+        <li>Mason Mount</li>
+        <li>Timo Werner</li>
+        <li>Christian Pulisic</li>
+    </ol>
+    <div>
+        Copyright © <?php echo date("Y"); ?> ESQ Business School
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,26 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/belajar', function (){
-    echo '<h1>Hello World</h1>';
-    
-}); 
-
-Route::get('/mahasiswa/{nama}', function ($nama){
-    return "Tampilkan mahasiswa bernama $nama";
-    
-}); 
-
-Route::get('/stok_barang/{jenis?}/{merek?}', function($a = 'smartphone',$b = 'samsung')
-{
-    return "Cek sisa stok untuk stok $a $b";
+Route::get('/mahasiswa', function (){
+    return view('universitas/mahasiswa')->with('mahasiswa01', 'Hakim Ziyech');
 });
 
-Route::get('/user/{id}',function ($id){
-    return "Tampilkan user dengan id = $id";
-})->where('id','[0-9]+');
-
-Route::get('/hubungi-kami', function (){
-    return '<h1>Hubungi Kami:</h1>';
-});
-Route::redirect('/contact-us','/hubungi-kami');
